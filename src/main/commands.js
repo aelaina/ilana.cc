17abb634fe7f5aa902387392f592da2645f4b1b3
aelaina <137407634+aelaina@users.noreply.github.com>
2024-04-11 20:10:00 +0100

    fuckass

--- src/main/commands.js
+++ src/main/commands.js
@@ -7,19 +7,17 @@ var commands = {
     },
     "about": () => {
         window.typist
-              .print("Welcome to my personal website! My name is Ilana (@aelaina).")
-              .print("Odit mollitia perspiciatis sint labore nobis reprehenderit odit. Et voluptatem harum iure voluptatem saepe.")
+              .print("Welcome to my bio site! My name is Ilana (@aelaina).")
+              .print("I'm a random trans (MtF) dumbass from the UK! I'm slowly learning my way through coding and other bigger projects over time!")
+              .print("A little more about me; I'm 17 and was born on december 26th, unlucky I know, however, I started my interest into coding once I first got into technology/pc hardware, was competent enough to build my first ever rig, from that point everything has been a new learning expereince!")
+              .print("I first started out with lua, which then evolved into other languages like html, c++ and more that I will learn overtime!")
               .print("You can find my contact information by running the 'contact' command.")
     },
     "contact": function(args) {
         window.typist.
             print('GitHub: <a href="https://github.com/aelaina">@aelaina</a>').
             sleep(100).
-            print('Telegram: <a href="https://t.me/...">@...</a>').
-            sleep(100).
             print('Discord: aelaina').
-            sleep(100).
-            print('LinkedIn: <a href="https://www.linkedin.com/in/...">...</a>').
             sleep(100).
             print('Email: email@example.com');
     },
